95cb82cd73ad44578aa0c6a3cffd40b2ff7d6370
Bernhard Vorhofer <b4hofer@gmail.com>
2025-07-31 13:09:43 +0200

    Add wofi, add powermenu, fix tmux window name

---
+++ .config/wofi/powermenu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+op=$( echo -e "  Poweroff\n  HybridSleep\n  Reboot\n  Suspend\n  Lock\n  Logout" | wofi --dmenu -j | awk '{print tolower($2)}' )
+
+case $op in 
+        poweroff)
+                ;&
+        reboot)
+                ;&
+        suspend)
+                systemctl $op
+                ;;
+        hybridsleep)
+                hyprlock && systemctl hybrid-sleep
+                ;;
+        lock)
+		hyprlock
+                ;;
+        logout)
+                hyprctl dispatch exit
+                ;;
+esac
+
