d79f4943e334dfb6efdaa16da0c7b7d103ed41fc
Bernhard Vorhofer <b4hofer@gmail.com>
2025-08-07 15:10:28 +0200

    wofi: Disable cache for powermenu to get consistent ordering

--- .config/wofi/powermenu.sh
+++ .config/wofi/powermenu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$( echo -e "  Poweroff\n  HybridSleep\n  Reboot\n  Suspend\n  Lock\n  Logout" | wofi --dmenu -j | awk '{print tolower($2)}' )
+op=$( echo -e "  Poweroff\n  HybridSleep\n  Reboot\n  Suspend\n  Lock\n  Logout" | wofi --dmenu -k /dev/null -j | awk '{print tolower($2)}' )
 
 case $op in 
         poweroff)
